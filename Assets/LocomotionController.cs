8a489c31d87c360a63696cf0db0d473cf934bd36
nuevakenia <54821755+nuevakenia@users.noreply.github.com>
2020-08-01 12:59:00 -0400

    Revert "0.20"
    This reverts commit bec88da11e6159bbf87623a4abb713b0fa7a255e.

--- Assets/LocomotionController.cs
+++ Assets/LocomotionController.cs
@@ -10,8 +10,6 @@ public class LocomotionController : MonoBehaviour
     public InputHelpers.Button teleportActivationButton;
     public float activationThreshold = 0.1f;
 
-    public XRRayInteractor leftInteractorRay;
-    public XRRayInteractor rightInteractorRay;
 
     public bool EnableLeftTeleport { get; set; } = true;
     public bool EnableRightTeleport { get; set; }  = true;
@@ -23,21 +21,14 @@ public class LocomotionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = new Vector3();
-        Vector3 norm = new Vector3();
-        int index = 0;
-        bool validTarget = false;
-
         if (leftTeleportRay)
         {
-            bool isLeftInteractorRayHovering = leftInteractorRay.TryGetHitInfo(ref pos, ref norm, ref index, ref validTarget);
-            leftTeleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftTeleportRay) && !isLeftInteractorRayHovering);
+            leftTeleportRay.gameObject.SetActive(EnableLeftTeleport && CheckIfActivated(leftTeleportRay));
         }
 
         if (rightTeleportRay)
         {
-            bool isRightInteractorRayHovering = rightInteractorRay.TryGetHitInfo(ref pos, ref norm, ref index, ref validTarget);
-            rightTeleportRay.gameObject.SetActive(EnableRightTeleport && CheckIfActivated(rightTeleportRay)&& !isRightInteractorRayHovering);
+            rightTeleportRay.gameObject.SetActive(EnableRightTeleport && CheckIfActivated(rightTeleportRay));
         }
     }
 
